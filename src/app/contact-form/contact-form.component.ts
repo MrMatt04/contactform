5ce3bef307dbc6dd0dfddc07c0d72e0d6c50fdd0
Mathieu van Tuil <mathieuvantuil2000@gmail.com>
2024-06-23 22:25:18 +0200

    finished group/form

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [ 
+    ReactiveFormsModule,
+  ],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
 
+  contactForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    phoneNum: new FormControl(''),
+    woonplaats: new FormControl(''),
+    adress: new FormControl(''),
+    Onderwerp: new FormControl(''),
+    bericht: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.warn(this.contactForm.value);
+  }
+
+
+  
   
 }
