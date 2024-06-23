5d710d983da62ec9e849349b43c8054756851433
Mathieu van Tuil <mathieuvantuil2000@gmail.com>
2024-06-24 00:08:05 +0200

    last commit

--- src/app/resultaten/resultaten.component.ts
+++ src/app/resultaten/resultaten.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { InMemoryDataService } from '../in-memory-data.service';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-resultaten',
@@ -8,5 +12,27 @@ import { Component } from '@angular/core';
   styleUrl: './resultaten.component.css'
 })
 export class ResultatenComponent {
+  private berichtenUrl = 'api/berichten';  // URL to web api
+  constructor(private http: HttpClient) { }
+
+  getBerichten(): Observable<InMemoryDataService[]> {
+    return this.http.get<InMemoryDataService[]>(this.berichtenUrl).pipe(
+      retry(2),
+      catchError((error: HttpErrorResponse) => {
+        console.error(error);
+        return throwError(error);
+      })
+    );
+  }
+  // createBericht(bericht: InMemoryDataService): Observable<InMemoryDataService> {
+  //   bericht.id = null;
+  //   return this.http.post<Product>(this.productsUrl, product).pipe(
+  //     catchError((error: HttpErrorResponse) => {
+  //       console.error(error);
+  //       return throwError(error);
+  //     })
+  //   )
+  // }
+
 
 }
